553b102ca5b0031c420674d217456556e482d350
akashshukla7458 <85392276+akashshukla7458@users.noreply.github.com>
2021-06-05 15:27:56 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer(){
+  var currYear = new Date().getFullYear();
+  return (
+    <footer>
+     <p>Copyright @ 2021</p>
+      </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note(){
+  return (
+    <div className="note">
+      <h1> Javascrpt and React.js </h1>
+      <p>This was an amazing  boot camp taken up by Shaurya sinha
+        we covered everything form scratch including javascript,
+        react.js , html.
+      </p>
+      </div>
+  );
+}
+export default Note;
